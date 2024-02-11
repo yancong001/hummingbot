cabb128f2e1d8656e45010229073f45b715699f7
bczhang <zhangbocheng@zhangboengdembp>
2024-02-12 02:41:41 +0800

    first add perp_xemm

---
+++ hummingbot/strategy/perp_cross_exchange_market_making/__init__.py
@@ -0,0 +1,8 @@
+from hummingbot.strategy.maker_taker_market_pair import MakerTakerMarketPair
+
+from .perp_cross_exchange_market_making import PerpCrossExchangeMarketMakingStrategy
+
+__all__ = [
+    MakerTakerMarketPair,
+    PerpCrossExchangeMarketMakingStrategy,
+]
